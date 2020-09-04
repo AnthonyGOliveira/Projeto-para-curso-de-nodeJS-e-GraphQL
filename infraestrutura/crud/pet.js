45c25d11810c2a69fd6d76faf701b8232c796047
Anthony <anthony.glivers@gmail.com>
2020-09-04 16:17:58 -0300

    Pets e Clientes
    Ligando tabelas de pets e clientes para que quando listar pets mostre também seus respectivos donos

--- infraestrutura/crud/pet.js
+++ infraestrutura/crud/pet.js
@@ -2,9 +2,24 @@ const executaQuery = require('../database/queries')
 
 class Pet {
   lista() {
-    const sql = 'SELECT * FROM Pets'
+    const sql = `SELECT Pets.id, Pets.nome, Pets.tipo, Pets.observacoes, 
+      Clientes.id as donoId, Clientes.nome as donoNome, Clientes.cpf as donoCpf 
+      FROM Pets INNER JOIN Clientes WHERE Pets.donoId = Clientes.id`
 
     return executaQuery(sql)
+      .then(pets => {
+        return pets.map(pet => ({
+          id: pet.id,
+          nome: pet.nome,
+          tipo: pet.tipo,
+          observacoes: pet.observacoes,
+          dono: {
+            id: pet.donoId,
+            nome: pet.donoNome,
+            cpf: pet.donoCpf
+          }
+        }))
+      })
   }
 
   buscaPorId(res, id) {
